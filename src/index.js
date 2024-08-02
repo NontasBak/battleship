59f97ba0d33cd22e3316c04a907cbc28377953e2
NontasBak <nontasbak@gmail.com>
2024-08-03 00:18:24 +0300

    Cleanup index.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ const player = new Player("player");
 const computer = new Player("computer");
 const display = new Display();
 
-display.displayBoard(player.gameBoard, "player");
 display.placeShipsDeterministic(player.gameBoard);
-display.displayBoard(player.gameBoard, "player");
 display.placeShipsDeterministic(computer.gameBoard);
+display.displayBoard(player.gameBoard, "player");
 display.displayBoard(computer.gameBoard, "computer");
